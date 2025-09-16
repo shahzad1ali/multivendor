a67b41124936fd45e7a97a6e78e5ec576cc1c3bd
shahzad1ali <shahzadmobi619@gmail.com>
2025-09-16 21:30:10 +0500

    replace packege lock

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,7 @@
-export const server = "https://multivender-nine.vercel.app/api/v2";
+export const server = "https://multivendor-6yt6.vercel.app/api/v2";
+
+export const backend_url = "https://multivendor-6yt6.vercel.app";
+
 // export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "https://multivender-nine.vercel.app";
 // export const backend_url = "https://localhost:8000";
