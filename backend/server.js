54b5449706139fcedab63fba5b829de3f93a3933
shahzad1ali <shahzadmobi619@gmail.com>
2025-09-17 10:32:44 +0500

    update server file

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,9 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 connectDB();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_SECRET_KEY,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // CREATE SERVER
