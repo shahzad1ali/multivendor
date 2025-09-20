5b1d85dca75817f157e28607ad59c5b138c2ef39
shahzad1ali <shahzadmobi619@gmail.com>
2025-09-20 11:24:50 +0500

    add vercel link

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -3,3 +3,4 @@ export const server = "https://multivendor-6yt6.vercel.app/api/v2";
 export const backend_url = "https://multivendor-6yt6.vercel.app";
 
 
+// export const backend_url = "http://localhost:8000";
